41b2ee269632c3d614e213d9722abf8def1cea81
songgaogy <gy31415926@qq.com>
2026-03-15 14:46:02 +0800

    minor modificaiton

--- lpb_robosuite/train_diffusion.sh
+++ lpb_robosuite/train_diffusion.sh
@@ -2,19 +2,10 @@ export CUDA_VISIBLE_DEVICES=0
 export WANDB_MODE="offline"
 export WANDB_ENTITY="songgao-personal"
 
-# python train.py \
-#   --config-name=train_diffusion_unet_hybrid_workspace \
-#   task=pandalift_image \
-#   task.dataset_path="/home/dodo/Documents/DAggar/robosuite/data/PandaLift/expert" \
-#   task.dataset.dataset_path="/home/dodo/Documents/DAggar/robosuite/data/PandaLift/expert" \
-#   training.device=cuda:0 \
-#   training.seed=42
-
-
 python train.py \
-  --config-dir=. \
-  --config-name=image_transport_diffusion_policy_cnn.yaml \
-  training.seed=42 \
+  --config-name=train_diffusion_unet_hybrid_workspace \
+  task=pandalift_image \
+  task.dataset_path="/home/dodo/Documents/DAggar/robosuite/data/PandaLift/expert" \
+  task.dataset.dataset_path="/home/dodo/Documents/DAggar/robosuite/data/PandaLift/expert" \
   training.device=cuda:0 \
-  hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}' \
-  dataset.dataset_path="/home/dodo/Documents/DAggar/robosuite/lpb_robosuite/data/transport/data/expert_demonstration"
+  training.seed=42
